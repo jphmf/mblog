167fe10cc77d6bc250cf5d082306ab693ccea2e6
pernambucano <demelofernandes.paulo@gmail.com>
2016-04-18 23:25:36 -0300

    Adicionando leitura a db

--- main.py
+++ main.py
@@ -18,53 +18,55 @@ import webapp2
 import os
 import jinja2
 
-from google.appengine.ext import db 
+from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
+
 
 class Post(db.Model):
-	title = db.StringProperty(required = True)
-	content = db.TextProperty(required = True)
-	created = db.DateTimeProperty(auto_now_add = True)
+    title = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
 
 
 class Handler(webapp2.RequestHandler):
-	def write(self, *a, **kw):
-		self.response.out.write(*a, **kw)
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
 
-	def render_str(self, template, **params):
-		t  = jinja_env.get_template(template)
-		return t.render(params)
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
 
-	def render(self, template, **kw):
-		self.write(self.render_str(template, **kw))
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
 
 
 class MainPage(Handler):
-	def get(self):
-		self.render("front.html")
+    def get(self):
+        self.render("front.html")
 
 
 class CreatePostHandler(Handler):
-	def render_form(self, title="", content="", error=""):
-		posts = db.GqlQuery("Select * from Post")
-		self.render("form.html", title=title, content=content, error=error)
+    def render_form(self, title="", content="", error=""):
+        posts = db.GqlQuery("Select * from Post")
+        self.render("form.html", title=title, content=content, error=error,
+                    posts=posts)
 
-	def get(self):
-		self.render_form()
+    def get(self):
+        self.render_form()
 
-	def post(self):
-		title = self.request.get("title")
-		content = self.request.get("content")
+    def post(self):
+        title = self.request.get("title")
+        content = self.request.get("content")
 
-		if title and content:
-			post = Post(title= title, content= content)
-			post.put()
-
-			self.redirect("/")
-		else:
-			error = "We need both a title and some artwork!"
-			self.render_form(title, content, error)
-
-app = webapp2.WSGIApplication([('/', MainPage),('/createPost', CreatePostHandler)], debug=True)
+        if title and content:
+            post = Post(title=title, content=content)
+            post.put()
+            self.redirect("/createPost")
+        else:
+            error = "We need both a title and some artwork"
+            self.render_form(title, content, error)
+app = webapp2.WSGIApplication([('/', MainPage),
+                              ('/createPost', CreatePostHandler)], debug=True)
